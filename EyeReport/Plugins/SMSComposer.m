448d442900a8cf2af2edd7958c56c5b06cfc8dc7
Landon Taylor <dno@Landons-MacBook-Pro-2.local>
2013-07-02 16:34:34 -0500

    changed some settings for composeSMS, no success yet

--- EyeReport/Plugins/SMSComposer.m
+++ EyeReport/Plugins/SMSComposer.m
@@ -18,38 +18,38 @@
 - (void)showSMSComposer:(NSArray *)arguments withDict:(NSDictionary *)options
 {
 	Class messageClass = (NSClassFromString(@"MFMessageComposeViewController"));
-    
+
 	if (messageClass != nil) {
 		if (![messageClass canSendText]) {
 			UIAlertView *alert = [[UIAlertView alloc]	initWithTitle	:@"Notice" message:@"SMS Text not available."
-                                                          delegate		:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+														delegate		:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
 			[alert show];
 			[alert release];
 			return;
 		}
 	} else {
 		UIAlertView *alert = [[UIAlertView alloc]	initWithTitle	:@"Notice" message:@"SMS Text not available."
-                                                      delegate		:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+													delegate		:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
 		[alert show];
 		[alert release];
 		return;
 	}
-    
+
 	NSString	*body = [options valueForKey:@"body"];
 	NSString	*toRecipientsString = [options valueForKey:@"toRecipients"];
-    
+
 	MFMessageComposeViewController *picker = [[MFMessageComposeViewController alloc] init];
-    
+
 	picker.messageComposeDelegate = self;
-    
+
 	if (body != nil) {
 		picker.body = [options valueForKey:@"body"];
 	}
-    
+
 	if (toRecipientsString != nil) {
 		[picker setRecipients:[toRecipientsString componentsSeparatedByString:@","]];
 	}
-    
+
 	[self.viewController presentModalViewController:picker animated:YES];
 	[[UIApplication sharedApplication] setStatusBarHidden:YES];	// /This hides the statusbar when the picker is presented -@RandyMcMillan
 	[picker release];
@@ -60,27 +60,27 @@
 {
 	// Notifies users about errors associated with the interface
 	int webviewResult = 0;
-    
+
 	switch (result) {
 		case MessageComposeResultCancelled:
 			webviewResult = 0;
 			break;
-            
+
 		case MessageComposeResultSent:
 			webviewResult = 1;
 			break;
-            
+
 		case MessageComposeResultFailed:
 			webviewResult = 2;
 			break;
-            
+
 		default:
 			webviewResult = 3;
 			break;
 	}
-    
+
 	[self.viewController dismissModalViewControllerAnimated:YES];
-    
+
 	NSString *jsString = [[NSString alloc] initWithFormat:@"window.plugins.smsComposer._didFinishWithResult(%d);", webviewResult];
 	[self writeJavascript:jsString];
 	[jsString release];
--- www/js/SMSComposer.js
+++ www/js/SMSComposer.js
@@ -4,6 +4,7 @@
  *
  * @constructor
  */
+
 function SMSComposer()
 {
 	this.resultCallback = null;
@@ -49,3 +50,53 @@ cordova.addConstructor(function() {
 					   }
 					   window.plugins.smsComposer = new SMSComposer();
 					   });
+
+/*
+ (function(cordova) {
+function SMSComposer()
+{
+	this.resultCallback = null;
+}
+
+SMSComposer.ComposeResultType =
+{
+Cancelled:0,
+Sent:1,
+Failed:2,
+NotSent:3
+}
+
+SMSComposer.prototype.showSMSComposer = function(toRecipients, body)
+{
+    
+	var args = {};
+    
+	if(toRecipients)
+		args.toRecipients = toRecipients;
+    
+	if(body)
+		args.body = body;
+    
+	cordova.exec("SMSComposer.showSMSComposer",args);
+}
+
+SMSComposer.prototype.showSMSComposerWithCB = function(cbFunction,toRecipients,body)
+{
+	this.resultCallback = cbFunction;
+	this.showSMSComposer.apply(this,[toRecipients,body]);
+}
+
+SMSComposer.prototype._didFinishWithResult = function(res)
+{
+	this.resultCallback(res);
+}
+
+cordova.addConstructor(function() {
+                       
+					   if(!window.plugins)	{
+					   window.plugins = {};
+					   }
+					   window.plugins.smsComposer = new SMSComposer();
+					   });
+})(window.cordova || window.Cordova);
+*/
