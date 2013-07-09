c06b3612d7b35fea7b64221ada37690d34ddf120
Landon Taylor <dno@Landons-MacBook-Pro-2.local>
2013-07-09 15:50:02 -0500

    modified SMSCompose.js

--- www/js/SMSComposer.js
+++ www/js/SMSComposer.js
@@ -3,7 +3,7 @@
  * window.plugins.SMSComposer
  *
  * @constructor
- */
+ *
 
 function SMSComposer()
 {
@@ -50,6 +50,64 @@ cordova.addConstructor(function() {
 					   }
 					   window.plugins.smsComposer = new SMSComposer();
 					   });
+*********/
+
+cordova.define("cordova/plugin/smsComposer", function(require, exports, module) {
+	var exec = require('cordova/exec');
+
+	var SMSComposer = function() {};
+
+	SMSComposer.prototype.showSMSComposer = function(success, failure, toRecipients, body) {
+
+		var args = {};
+    
+	if(toRecipients)
+		args.toRecipients = toRecipients;
+    
+	if(body)
+		args.body = body;
+
+	   exec(success, failure, 'SMSComposer', 'showSMSComposer', [toRecipients, body]);
+	}
+
+	SMSComposer.prototype.showSMSComposerWithCB = function(success, failure, cbFunction, toRecipients,body) {
+
+	   	this.resultCallback = cbFunction;
+		this.showSMSComposer.apply(this,[toRecipients,body]);
+		exec(success, failure,'SMSComposer', 'showSMSComposerWithCB', [cbFunction, toRecipients, body]);
+	}
+
+	SMSComposer.prototype._didFinishWithResult = function(success, failure, res) {
+	   this.resultCallback(res);
+	   exec(success, failure, 'SMSComposer', '_didFinishWithResult', [res]);
+	}
+
+	var smsComposer = new SMSComposer();
+	module.exports = smscomposer;
+});
+
+
+// ///////////////////
+(function(gap){
+// ///////////////////
+
+// YOUR CODE HERE! 
+// NOTE: Replace any PhoneGap, Cordova or cordova references with "gap" variable above
+
+// the variables below support old to new references ///////////////////
+})(window.PhoneGap || window.Cordova || window.cordova);
+// ///////////////////
+
+
+/* //usage:
+   var mp = cordova.require("cordova/plugin/myplugin");
+   var win  = function() { alert('win!');  }
+   var fail = function() { alert('fail!'); }
+
+   mp.foo(win, fail);
+   mp.bar(win, fail);
+   mp.baz(win, fail);
+*/
 
 /*
  (function(cordova) {
@@ -98,5 +156,10 @@ cordova.addConstructor(function() {
 					   }
 					   window.plugins.smsComposer = new SMSComposer();
 					   });
-})(window.cordova || window.Cordova);
-*/
+
+//window.smsComposer = new SMSComposer();
+
+})	(window.cordova || window.Cordova);
+ */
+
+ 
