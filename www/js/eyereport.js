afbacf2944d9f7f3252e2ad7ce9ce7894bffd595
Landon Taylor <dno@Landons-MacBook-Pro-2.local>
2013-07-09 19:14:07 -0500

    added section at bottom of page with geolocation coordinates, using for testing and will remove once it is passed to the SMS body with google map link

--- www/js/eyereport.js
+++ www/js/eyereport.js
@@ -1,3 +1,39 @@
+ /******* Request access to Location Services *******/
+            
+            // Wait for PhoneGap to load
+            //
+            document.addEventListener("deviceready", onDeviceReady, false);
+             var watchID = null;
+            // PhoneGap is ready
+            //
+            function onDeviceReady() {
+             //   navigator.geolocation.getCurrentPosition(onSuccess, onError);
+                // onSuccess Geolocation
+                var options = { timeout: 3000000 };
+                watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+            }
+
+                function onSuccess(position) {
+                    var element = document.getElementById('geolocation');
+                    element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+                    'Longitude: '          + position.coords.longitude             + '<br />' +
+                    'Altitude: '           + position.coords.altitude              + '<br />' +
+                    'Accuracy: '           + position.coords.accuracy              + '<br />' +
+                    'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+                    'Heading: '            + position.coords.heading               + '<br />' +
+                    'Speed: '              + position.coords.speed                 + '<br />' +
+                    'Timestamp: '          + new Date(position.timestamp)          + '<br />';
+                }
+                
+                // onError Callback receives a PositionError object
+                //
+                function onError(error) {
+                    alert('code: '    + error.code    + '\n' +
+                          'message: ' + error.message + '\n');
+                }
+
+/////////////////////
+/*
 
             // Wait for Cordova to load
             //
@@ -29,16 +65,16 @@
             
             // Cordova is ready
             //
-            function onSuccess(position) {
-           /****** routes current location to specified coordinate... pops up onDevicce ready need to fix
+   //         function onSuccess(position) {
+           // routes current location to specified coordinate... pops up onDevicce ready need to fix
             
-                var ref = window.open('https://maps.google.com/maps?saddr='+position.coords.latitude+','+position.coords.longitude+'&daddr=45.404232,11.926511', '_blank', 'location=yes');
-                ref.addEventListener('loadstart', function() { alert('start: ' + event.url); });
-                ref.addEventListener('loadstop', function() { alert('stop: ' + event.url); });
-                ref.addEventListener('exit', function() { alert(event.type); });
+           //     var ref = window.open('https://maps.google.com/maps?saddr='+position.coords.latitude+','+position.coords.longitude+'&daddr=45.404232,11.926511', '_blank', 'location=yes');
+           //     ref.addEventListener('loadstart', function() { alert('start: ' + event.url); });
+           //     ref.addEventListener('loadstop', function() { alert('stop: ' + event.url); });
+           //     ref.addEventListener('exit', function() { alert(event.type); });
             
-            **********/
-            }
+            
+    //        }
 
 
 $("#getinfo").click(function() {
@@ -54,3 +90,4 @@ $('input[name=capture_type]').change(function() {
                                      alert('Radio Value: '+radio_val);
                                      });
 
+*/
