f777bc99314081a4c844f6bf877b3f328642229f
Landon Taylor <dno@Landons-MacBook-Pro-2.local>
2013-07-18 04:16:08 -0500

    added instruction screen files and code

--- www/js/eyereport.js
+++ www/js/eyereport.js
@@ -16,8 +16,10 @@
                 function onSuccess(position) {
                     var element = document.getElementById('geolocation');
                     element.innerHTML = "I need help, call me now! "+"http://maps.google.com/maps?q="+position.coords.latitude+","+position.coords.longitude+"(My+Point)&z=14&ll="+position.coords.latitude+","+position.coords.longitude;
-                    var loc = document.getElementById('entry_423085064');
-                    loc.value = position.coords.latitude+","+position.coords.longitude;
+                    var lon = document.getElementById('entry_916979227');
+                    lon.value = position.coords.longitude;
+                    var lat = document.getElementById('entry_423085064');
+                    lat.value = position.coords.latitude;                    
                     /*
                     var origPos = "location";
                     var geoPos = origPos.replace("location", position.coords.latitude + position.coords.longitude);
--- www/js/post.js
+++ www/js/post.js
@@ -6,12 +6,14 @@
             //var spanValue  = $('#camera_status').text();
             var input = document.getElementById('entry_1485650557_other_option_response');//.value; 
                          input = entry_1485650557_other_option_response.value;
-            var loc = document.getElementById('entry_423085064');
-                        loc = entry_423085064.value;             
+            var lon = document.getElementById('entry_916979227');
+                        lon = entry_916979227.value; 
+            var lat = document.getElementById('entry_423085064');
+                        lat = entry_423085064.value;                         
                          
             $.ajax({
                 type: 'POST',
-                data: postData +'&lid='+landmarkID + input + loc,
+                data: postData +'&lid='+landmarkID + input + lon + lat,
                 //change the url for your project
                 //'https://docs.google.com/forms/d/1MfiapJ3D_wYS2njNiVUYnwh6nAfqN-mVXR5-7ihKLXU/formResponse', new
                 //'https://docs.google.com/forms/d/1Ngovqo8yr-SoJ93XaGOGFibmWhxUOVklztHz8IYhh_g/formResponse', orig
